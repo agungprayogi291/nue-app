6829b1c9f249385d5397112def9362f0a4053de6
agung <agung.visione@gmail.com>
2023-03-28 09:35:59 +0700

    update:checkout

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -1,12 +1,15 @@
 import Navigation from "../components/navigation";
 import Footer from "../components/footer";
 import Order from "../components/order";
+import Checkout from '../components/checkout/checkout';
 function Orders(){
     return(
         <>
             <Navigation bg='bg-dark text-white'/>
-            <div className="container">
-                <Order />
+            <div className="container my-5">
+                <div className="row py-3">
+                    <Checkout />
+                </div>
             </div>
        
             
